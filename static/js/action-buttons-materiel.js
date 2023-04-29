c9da48c2cffa694d5c634fdfdbd55b10c8f9f854
InstaZDLL <github.105mh@8shield.net>
2023-04-29 11:44:55 +0200

    Add the filter button code to the action-buttons-material.js

--- static/js/action-buttons-materiel.js
+++ static/js/action-buttons-materiel.js
@@ -1,3 +1,23 @@
+// Filter button
+$(document).ready(function() {
+    var columnsHidden = false;
+    $("#hideColumns").click(function() {
+        if (columnsHidden) {
+            // Show the date_achat and date_expi columns
+            $("th:contains('Date of Purchase'), th:contains('Expiration Date')").show();
+            $("td:nth-child(5), td:nth-child(6)").show();
+            $(this).text("Hide Columns");
+            columnsHidden = false;
+        } else {
+            // Hide the date_achat and date_expi columns
+            $("th:contains('Date of Purchase'), th:contains('Expiration Date')").hide();
+            $("td:nth-child(5), td:nth-child(6)").hide();
+            $(this).text("Unhide");
+            columnsHidden = true;
+        }
+    });
+});
+// Delete Button
 $(document).ready(function() {
     $('.delete-b').click(function() {
         var row_id = $(this).data('row-id');

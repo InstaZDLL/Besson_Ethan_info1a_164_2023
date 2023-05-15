b57feeea0eb33a449f57daca515f4f909da4e272
InstaZDLL <github.105mh@8shield.net>
2023-05-15 13:45:36 +0200

    "Add about route blueprint and page to Flask application."

--- FlaskWebS/__init__.py
+++ FlaskWebS/__init__.py
@@ -4,8 +4,10 @@ app = Flask(__name__, static_url_path='/static', template_folder='templates')
 
 # Import and register the route blueprint
 from FlaskWebS.routes.index_route import bp as index_bp
+from FlaskWebS.routes.about_route import bp as about_bp
 
 app.register_blueprint(index_bp)
+app.register_blueprint(about_bp)
 
 # Other configuration and code for your Flask application
 # ...

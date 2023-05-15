67d8dfe3033a83a57c12efe94fb4ffdb11e71f7e
InstaZDLL <github.105mh@8shield.net>
2023-05-15 22:25:11 +0200

    Add startup messages and install dependencies in start scripts.

--- start.bat
+++ start.bat
@@ -1,37 +1,7 @@
 @echo off
 echo "Démarrage en cours ..."
+echo "Installations des dépendances ..."
+pip install -r requirements.txt
 python.exe run_app.py
 echo "Fin du programme"
 pause
-
-
-
-REM @echo off
-REM title Animation
-
-REM set count=0
-REM cls
-
-REM echo Demarrage en cours ...
-
-REM :loop
-REM set /A count+=1
-
-REM if %count%==1 (
-  REM set "dots=."
-REM ) else if %count%==2 (
-  REM set "dots=.."
-REM ) else if %count%==3 (
-  REM set "dots=..."
-REM )
-
-setlocal enabledelayedexpansion
-set "message=Demarrage en cours %dots%"
-echo !message!
-
-if %count%==3 (
-  set count=0
-)
-
-timeout 1 > nul
-goto loop

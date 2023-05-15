8f8a4ce858c6aef576b92713836d978699e9c717
InstaZDLL <github.105mh@8shield.net>
2023-05-15 13:42:41 +0200

    Refactor code to import Flask and Blueprint in one line and fix syntax errors related to index_route.

--- FlaskWebS/__init__.py
+++ FlaskWebS/__init__.py
@@ -4,6 +4,7 @@ app = Flask(__name__, static_url_path='/static', template_folder='templates')
 
 # Import and register the route blueprint
 from FlaskWebS.routes.index_route import bp as index_bp
+
 app.register_blueprint(index_bp)
 
 # Other configuration and code for your Flask application
--- FlaskWebS/routes/index_route.py
+++ FlaskWebS/routes/index_route.py
@@ -2,6 +2,7 @@ from flask import render_template, Blueprint
 
 bp = Blueprint('index', __name__)
 
+
 @bp.route('/')
 def index():
     """

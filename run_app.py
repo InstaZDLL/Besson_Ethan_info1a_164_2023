caf7f2e68b2b272abfde0f0fe5f05b0ec0c287a8
InstaZDLL <github.105mh@8shield.net>
2023-05-15 15:53:23 +0200

    Refactor run_app.py to include server address, port, and debug mode in the app.run() call.

--- run_app.py
+++ run_app.py
@@ -1,4 +1,6 @@
-from FlaskWebS import app
+from FlaskWebS import app, adresse_srv_flask,port_flask,debug_flask
 
 if __name__ == '__main__':
-    app.run()
+    # context = ('ssl/cert.crt', 'ssl/cert.key')
+    # ssl_context=context
+    app.run(host=adresse_srv_flask, port=port_flask, debug=debug_flask)

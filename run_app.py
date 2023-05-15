558ccdd1d181467b189330dd4713bcd1c40ddf67
InstaZDLL <github.105mh@8shield.net>
2023-05-16 00:43:31 +0200

    Refactor run_app.py to enable https and avoid double execution of mysql_dump_import.

--- run_app.py
+++ run_app.py
@@ -1,11 +1,10 @@
-from FlaskWebS import app, adresse_srv_flask,port_flask,debug_flask
+from FlaskWebS import app, adresse_srv_flask, port_flask, debug_flask
 from FlaskWebS.database import mysql_dump_import
 
-
-
 if __name__ == '__main__':
+
     """
-        If you want to enable the https, trust the crt
+        If you want to enable the https, trust the crt file to Trusted Root Certificates and uncomment these two lines
         context = ('ssl/cert.crt', 'ssl/cert.key')
         ssl_context=context
     """

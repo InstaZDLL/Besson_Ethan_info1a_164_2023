0ec5afd2d79c6cf0bc7ac398c066b37506bece62
InstaZDLL <github.105mh@8shield.net>
2023-06-02 23:40:28 +0200

    Change start.sh to execute run_app.py instead of run_app.pycls.

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ echo "Démarrage en cours ..."
 echo "Installations des dépendances ..."
 python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
-python3 run_app.pycls
+python3 run_app.py
 echo "Fin du programme"
